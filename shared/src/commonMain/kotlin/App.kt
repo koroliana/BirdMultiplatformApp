26d5ddb6e4a159ce00ac865e5a8597c17ea99e40
Koroleva Elena <koroliana@yandex.ru>
2024-08-12 19:47:38 +0700

    initial commit

---
+++ shared/src/commonMain/kotlin/App.kt
@@ -0,0 +1,41 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun App() {
+    MaterialTheme {
+        var greetingText by remember { mutableStateOf("Hello, World!") }
+        var showImage by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = {
+                greetingText = "Hello, ${getPlatformName()}"
+                showImage = !showImage
+            }) {
+                Text(greetingText)
+            }
+            AnimatedVisibility(showImage) {
+                KamelImage(
+                    asyncPainterResource("https://sebi.io/demo-image-api/pigeon/shaquon-gibson-9Q3UL6wGjts-unsplash.jpg"),
+                    "Pigeon"
+                )
+            }
+        }
+    }
+}
+
+expect fun getPlatformName(): String
